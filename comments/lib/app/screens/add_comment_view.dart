d5478adbe8d3e40e1c08c514d8aec3b00cb23aa7
devloop100 <88592306+devloop100@users.noreply.github.com>
2022-06-07 16:44:33 +0300

    added international text direction
    text field to send comment now supports hebrew text

--- comments/lib/app/screens/add_comment_view.dart
+++ comments/lib/app/screens/add_comment_view.dart
@@ -1,7 +1,36 @@
+import 'package:comments/common/widgets/text_direction.dart';
 import 'package:flutter/material.dart';
 
-class AddCommentView extends StatelessWidget {
+class AddCommentView extends StatefulWidget {
   const AddCommentView({Key? key}) : super(key: key);
+
+  @override
+  State<AddCommentView> createState() => _AddCommentViewState();
+}
+
+class _AddCommentViewState extends State<AddCommentView> {
+  final TextEditingController _controller = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(() {
+      _textUpdated();
+    });
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    _controller.dispose();
+    super.dispose();
+  }
+
+  // update the state when text is change so the auto direction can be updated
+  void _textUpdated() {
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +39,42 @@ class AddCommentView extends StatelessWidget {
         title: const Text('Add New Comment'),
       ),
       resizeToAvoidBottomInset: false,
-      body: Container(),
+      body: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.35,
+            padding: const EdgeInsets.all(16),
+            child: AutoDirection(
+              text: _controller.text,
+              child: TextFormField(
+                focusNode: _focusNode,
+                autofocus: true,
+                controller: _controller,
+                maxLines: 12,
+                keyboardType: TextInputType.multiline,
+                style: const TextStyle(color: Colors.black),
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                // _futureAlbum = createAlbum(_controller.text);
+              });
+            },
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Text('Send Comment'),
+                SizedBox(
+                  width: 10,
+                ),
+                Icon(Icons.send)
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
