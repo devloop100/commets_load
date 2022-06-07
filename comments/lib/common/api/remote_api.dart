d94538b40cf5f5c272e124db86ff137d29682f2b
devloop100 <88592306+devloop100@users.noreply.github.com>
2022-06-07 17:38:00 +0300

    replaced http with Dio for api calls
    as required by the assignment

--- comments/lib/common/api/remote_api.dart
+++ comments/lib/common/api/remote_api.dart
@@ -1,63 +1,49 @@
-import 'dart:convert';
 import 'dart:io';
 
 import 'package:comments/app/models/comment_model.dart';
 import 'package:comments/main.dart';
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 
 // class for remote api to fetch the comments from the server and return the list of comments
 // ignore: avoid_classes_with_only_static_members
 class RemoteApi {
   static Future<List<CommentModel>> getCommentsList(
-          int offset, int limit) async =>
-      http
-          .get(
-            _ApiUrlBuilder.commentsList(offset, limit),
-          )
-          .mapFromResponse<List<CommentModel>, List<dynamic>>(
-            (jsonArray) => _parseItemListFromJsonArray(
-              jsonArray,
-              (jsonObject) => CommentModel.fromJson(jsonObject),
-            ),
-          );
-
-  static List<T> _parseItemListFromJsonArray<T>(
-    List<dynamic> jsonArray,
-    T Function(dynamic object) mapper,
-  ) =>
-      jsonArray.map(mapper).toList();
-}
-
-class GenericHttpException implements Exception {}
-
-class NoConnectionException implements Exception {}
-
-// build the url to fetch the comments list from the api server with the given offset and limit
-// ignore: avoid_classes_with_only_static_members
-class _ApiUrlBuilder {
-  static Uri commentsList(
-    int offset,
-    int limit,
-  ) =>
-      Uri.parse(
-        '$baseUrl?'
-        '_page=$offset'
-        '&_limit=$limit',
-      );
-}
-
-// map the response from the api server to a list of items or throw an exception if the response is not valid
-extension on Future<http.Response> {
-  Future<R> mapFromResponse<R, T>(R Function(T) jsonParser) async {
+      int offset, int limit) async {
     try {
-      final response = await this;
-      if (response.statusCode == 200) {
-        return jsonParser(jsonDecode(response.body));
+      // map the response from the api server to a list of items or throw an exception if the response is not valid
+      var response = await Dio().get(
+        commentsListUrl(offset, limit),
+      );
+      if (response.statusCode == HttpStatus.ok) {
+        return _parseItemListFromJsonArray(
+          response.data,
+          (jsonObject) => CommentModel.fromJson(jsonObject),
+        );
       } else {
-        throw GenericHttpException();
+        throw GenericHttpException;
       }
     } on SocketException {
       throw NoConnectionException();
     }
   }
+
+  // helper method to parse the json array to a list of items
+  static List<T> _parseItemListFromJsonArray<T>(
+    List<dynamic> jsonArray,
+    T Function(dynamic object) mapper,
+  ) =>
+      jsonArray.map(mapper).toList();
+
+  // build the url to fetch the comments list from the api server with the given offset and limit
+  static String commentsListUrl(
+    int offset,
+    int limit,
+  ) =>
+      '$baseUrl?'
+      '_page=$offset'
+      '&_limit=$limit';
 }
+
+class GenericHttpException implements Exception {}
+
+class NoConnectionException implements Exception {}
